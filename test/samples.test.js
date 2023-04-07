c7694441139d86fe08e6606ddcd23f7fbb250a7c
Nicola Peduzzi <thenikso@gmail.com>
2023-04-07 13:18:36 +0200

    Add support for `return`

--- test/samples.test.js
+++ test/samples.test.js
@@ -30,4 +30,20 @@ describe('Samples', async (assert) => {
     'Final: ',
     'Account balance: $10\n',
   );
+
+  assertLogs(
+    String.raw`#!/usr/bin/env io
+
+    ack := method(m, n,
+      //writeln("ack(", m, ",", n, ")")
+      if (m < 1, return n + 1)
+      if (n < 1, return ack(m - 1, 1))
+      return ack(m - 1, ack(m, n - 1))
+    )
+
+    ack(3, 4) print
+    "\n" print
+    `,
+    '125', '\n',
+  );
 });
